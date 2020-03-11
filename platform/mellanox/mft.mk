5f7f38e48f165accedb268c29d1b968b4864c1eb
Stephen Sun <stephens@mellanox.com>
2020-03-11 16:44:20 +0800

    [Mellanox]Support buster Currently we port SONiC to buster in a way that building docker-base based on buster and other dockers based on stretch. The benefit is that tasks can be carried out simultaneously. The build procedure can be treated as 2 stages. The first stage is to build the stretch-based debs and dockers and the second stage is to build the buster-based ones. One thing we have to pay attention to is some debs depend on kernel should not be built at stretch stage because the kernel isn't available at that time. The idea is to move that kind of debs out of SONIC_STRETCH_DEBS and SONIC_MAKE_DEBS and add them to SONIC_DPKG_DEBS. Meanwhile, any dependency explicitly put on the stretch based dockers on kernel should be removed.

--- platform/mellanox/mft.mk
+++ platform/mellanox/mft.mk
@@ -8,12 +8,10 @@ export MFT_VERSION MFT_REVISION
 MFT = mft_$(MFT_VERSION)-$(MFT_REVISION)_amd64.deb
 $(MFT)_SRC_PATH = $(PLATFORM_PATH)/mft
 $(MFT)_DEPENDS += $(LINUX_HEADERS) $(LINUX_HEADERS_COMMON)
-SONIC_MAKE_DEBS += $(MFT)
+SONIC_DPKG_DEBS += $(MFT)
 
 KERNEL_MFT = kernel-mft-dkms_$(MFT_VERSION)-$(KVERSION)_all.deb
 $(eval $(call add_derived_package,$(MFT),$(KERNEL_MFT)))
 
 MFT_OEM = mft-oem_$(MFT_VERSION)-$(MFT_REVISION)_amd64.deb
 $(eval $(call add_derived_package,$(MFT),$(MFT_OEM)))
-
-SONIC_STRETCH_DEBS += $(KERNEL_MFT)
