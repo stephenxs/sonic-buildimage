90d9b1dc0cb81eca53caa96845ffeca15aec8fe2
Stephen Sun <stephens@nvidia.com>
2022-08-26 09:16:00 +0000

    Rename platform API
    Signed-off-by: Stephen Sun <stephens@nvidia.com>

--- platform/mellanox/mlnx-platform-api/sonic_platform/psu.py
+++ platform/mellanox/mlnx-platform-api/sonic_platform/psu.py
@@ -540,7 +540,7 @@ class Psu(FixedPsu):
 
         return None
 
-    def get_psu_power_threshold(self):
+    def get_psu_power_warning_threshold(self):
         """
         Retrieve the warning threshold of the power on this PSU
         The value can be volatile, so the caller should call the API each time it is used.
