c579c8c37a2e9670174fa2dafef47e4edf979d5c
Stephen Sun <stephens@nvidia.com>
2023-07-17 13:59:08 +0800

    Use single thread to avoid timing issue
    Signed-off-by: Stephen Sun <stephens@nvidia.com>

--- dockers/docker-orchagent/monitor_pfc_counters.py
+++ dockers/docker-orchagent/monitor_pfc_counters.py
@@ -8,7 +8,6 @@ import time
 fetch_counter_script = \
 '''
 local counters_db = "2"
-local config_db = "4"
 
 local portoidmap = {}
 local result = {}
@@ -46,65 +45,47 @@ return qid2pid
 
 counters = []
 
-class CounterPollingThread(threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self)
-        self.name = "CounterPolling"
-        self.task_stopping_event = threading.Event()
-        self.redis = redis.Redis(host='localhost', db=2)
-        self.sha = self.redis.script_load(fetch_counter_script)
-
-    def signal_handler(self, sig, frame):
-        if sig == signal.SIGINT:
-            self.task_stopping_event.set()
-        elif sig == signal.SIGTERM:
-            self.task_stopping_event.set()
-
-    def run(self):
-        global counters
-        while not self.task_stopping_event.is_set():
-            counters.append(self.redis.evalsha(self.sha, 0))
-            if len(counters) > 50:
-                counters.remove(counters[0])
-            time.sleep(0.2)
-
-    def join(self):
-        self.task_stopping_event.set()
-        threading.Thread.join(self)
-
 
 def fetch_counter_for_port(allport_counters, portoid):
     for counter in allport_counters:
         if 'COUNTERS:' + portoid == counter[1]:
             return counter
+    return None
     
-cpt = CounterPollingThread()
-cpt.start()
 
 r = redis.Redis(host='localhost', db=2)
-sha = r.script_load(fetch_qid_pid_map_script)
-qid_pid_list = r.evalsha(sha, 0)
+qid_pid_map_sha = r.script_load(fetch_qid_pid_map_script)
+qid_pid_list = r.evalsha(qid_pid_map_sha, 0)
 qid2pid = dict(list(zip(qid_pid_list[0::2], qid_pid_list[1::2])))
+
+fetch_counter_sha = r.script_load(fetch_counter_script)
 
 ps = r.pubsub()
 ps.subscribe('PFC_WD_ACTION')
-it = ps.listen()
 
-history_counters = []
-for i in it:
-    # {'pattern': None, 'type': 'message', 'channel': 'PFC_WD_ACTION', 'data': '["oid:0x1500000000080e","storm"]'}
-    if not i:
-        continue
-    data = i.get('data')
-    if not isinstance(data, (str)):
-        continue
-    items = data[1:-1].split(',')
-    if 'storm' == items[1][1:-1]:
-        portoid = qid2pid[items[0][1:-1]]
-        counters_snapshot = counters[:]
-        for allport_counters in counters_snapshot:
-            history_counters.append(fetch_counter_for_port(allport_counters, portoid))
+while True:
+    counters.append(r.evalsha(fetch_counter_sha, 0))
+    if len(counters) > 50:
+        counters.pop(0)
+    notification = ps.get_message()
+    if notification:
+        data = notification.get('data')
+        if isinstance(data, (str)):
+            items = data[1:-1].split(',')
+            if 'storm' == items[1][1:-1]:
+                history_counters = []
+                try:
+                    portoid = qid2pid[items[0][1:-1]]
+                    counters_snapshot = counters[:]
+                    for allport_counters in counters:
+                        counter_info = fetch_counter_for_port(allport_counters, portoid)
+                        if counter_info:
+                            if len(history_counters) == 0:
+                                history_counters.append(counter_info[0])
+                                history_counters.append(counter_info[1])
+                            history_counters.append(counter_info[2])
 
-        history_counters.append(fetch_counter_for_port(counters[-1], portoid))
-        print('Historical counters {}'.format(history_counters))
-        history_counters = []
+                finally:
+                    print('Historical counters {}'.format(history_counters))
+
+    time.sleep(0.2)
